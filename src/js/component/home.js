3b874e02ec1154bcaadb01d1ba6e64e23a9cdc83
xenogenesis20 <xenogenesis20@outlook.com>
2021-05-14 19:46:56 +0000

    more stuff

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,69 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+// Create an option to countdown from a given number.
+// Create stop, reset, and resume functionality
+// Create an alert when the user reaches a specified time, ie the user enters "10", an alert should render notifying the user that their time was reached
+export function Home(props) {
+	const [userNumber, setUserNumber] = useState(0);
+	const [isRunning, setRunning] = useState(true);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<button
+				onClick={() => {
+					setRunning(true);
+					props.getState(isRunning);
+				}}>
+				Play
+			</button>
+			<button
+				onClick={() => {
+					props.pauseFunc();
+				}}>
+				Pause
+			</button>
+			<button
+				onClick={() => {
+					setUserNumber(0);
+					props.myFunction(userNumber);
+				}}>
+				Reset
+			</button>
+			<button
+				onClick={() => {
+					props.myFunction(userNumber);
+				}}>
+				set num
+			</button>
+			<input
+				type="number"
+				placeholder="pick a 6 digit number"
+				onChange={e => {
+					setUserNumber(e.target.value);
+				}}
+			/>
+			<div className="container d-flex">
+				<div>{props.number1}</div>
+				<div>{props.number2}</div>
+				<div>{props.number3}</div>
+				<div>{props.number4}</div>
+				<div>{props.number5}</div>
+				<div>{props.number6}</div>
+			</div>
+		</>
 	);
 }
+
+Home.propTypes = {
+	number1: PropTypes.string,
+	number2: PropTypes.string,
+	number3: PropTypes.string,
+	number4: PropTypes.string,
+	number5: PropTypes.string,
+	number6: PropTypes.string,
+	myFunction: PropTypes.func,
+	getState: PropTypes.func,
+	pauseFunc: PropTypes.func
+};
--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//include bootstrap npm library into the bundle
 import "bootstrap";
 
-//include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// Create an option to countdown from a given number.
+let num = 0;
+function setNum(number) {
+	num = parseInt(number);
+}
+let currentState = true;
+function stateHandler(state) {
+	currentState = state;
+}
+
+function clearTimeout() {
+	clearInterval(timer);
+}
+
+function counter() {
+	num -= 1;
+	let string = "00000" + num;
+	let num2 = string.slice(-6);
+	if (currentState) {
+		timer;
+	}
+
+	ReactDOM.render(
+		<Home
+			number1={num2.charAt(0)}
+			number2={num2.charAt(1)}
+			number3={num2.charAt(2)}
+			number4={num2.charAt(3)}
+			number5={num2.charAt(4)}
+			number6={num2.charAt(5)}
+			myFunction={setNum}
+			getState={stateHandler}
+			pauseFunc={clearTimeout}
+		/>,
+
+		document.querySelector("#app")
+	);
+}
+let timer = setInterval(counter, 1000);
